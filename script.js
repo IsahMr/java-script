f5749c9eaee3788e63021aefa54e1c4487d048a3
IsahMr <160750926+IsahMr@users.noreply.github.com>
2024-03-01 11:36:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudaTexto(){
+    document.getElementById("demo").innerHTML = "Hello JavaScript!"
+}
+
+function mostraHora(){
+    document.getElementById('paragrafo2').innerHTML=Date()
+}
